b6a4eb191a3fdca94094b9662366c0c4a0ba709f
ThisIsSilv <54795019+ThisIsSilv@users.noreply.github.com>
2020-03-04 13:58:16 +0100

    Add files via upload

---
+++ naamloze map/002/scripts/2_1.js
@@ -0,0 +1,9 @@
+var mijnauto ={
+    merk: "Mini Cooper",
+    Type: "Klein",
+    aantalWielen: 4,
+    kleur: "Geel"
+}
+
+console.log(mijnauto.kleur);
+document.getElementById("element").innerHTML = mijnauto.kleur
---
+++ naamloze map/002/scripts/vulmetjs.js
@@ -0,0 +1,5 @@
+document.getElementById("paginatitel").innerHTML = "Pagina met swagg"
+document.getElementById("subkop").innerHTML = "subkop met swagg"
+document.getElementById("img1").src = "img/img1.jpg"
+document.getElementById("img2").src = "img/img2.png"
+
